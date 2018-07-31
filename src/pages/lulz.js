0ea5b57c3007c2e3e37ea1c16c2b34b30436f67e
Piotr F <piotr@piotrf.pl>
2018-07-31 10:40:54 +0200

    Set up the 2 landing pages.

---
+++ src/pages/lulz.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from "react";
+
+export default class LulzLandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Lulz</h1>
+      </Fragment>
+    );
+  }
+}
+
+LulzLandingPage.propTypes = {};
---
+++ src/pages/seriously.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from "react";
+
+export default class SeriousLandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Serious landing page</h1>
+      </Fragment>
+    );
+  }
+}
+
+SeriousLandingPage.propTypes = {};
