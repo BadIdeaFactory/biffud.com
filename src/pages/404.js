cbb1f72398ea0193682983afbf3d565954ca5c1a
Lou Huang <lou@streetmix.net>
2024-12-03 09:46:15 -0500

    refactor(404): add title to iframe + refactor component
    - resolve a warning that iframe is missing title attribute
    - remove deprecated scrolling and frameborder attributes, use css instead
    - refactor component to react function component
    - keep the background music on this page even though it will never play unless
      you knew it was there and specifically allowed this site to play bg music

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,27 @@
-import React, { Component, Fragment } from "react";
+import React from "react";
+import styled from "styled-components";
 
 import { Helmet, Layout } from "ui/partials";
 
-export default class ErrorPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Iframe = styled.iframe`
+  overflow: hidden;
+  border: 0;
+`;
 
-  render() {
-    return (
-      <Fragment>
-        <Helmet {...this.props} title="Error" />
-        <Layout {...this.props}>
-          <h1>You'd think there would be something more interesting here.</h1>
-          <iframe width="1" height="1" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/348238181&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-        </Layout>
-      </Fragment>
-    );
-  }
+export default function ErrorPage(props) {
+  return (
+    <>
+      <Helmet {...props} title="Error" />
+      <Layout {...props}>
+        <h1>You'd think there would be something more interesting here.</h1>
+        <Iframe
+          title="Background music"
+          width="1"
+          height="1"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/348238181&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        />
+      </Layout>
+    </>
+  );
 }
-
-ErrorPage.propTypes = {};
