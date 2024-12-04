6f70baaaf9934ab9f4f63a8f57dd3302b4e4f261
Lou Huang <lou@streetmix.net>
2024-12-04 13:52:17 -0500

    refactor(contexts): port typescript

--- lib/ui/contexts/SharedHEX.context.ts
+++ lib/ui/contexts/SharedHEX.context.ts
@@ -1,4 +1,7 @@
 import { createContext } from "react";
 
-export const SharedHexContext = createContext();
+export const SharedHexContext = createContext({
+  BIFHEX: ''
+});
+
 export const SharedHexConsumer = SharedHexContext.Consumer;
