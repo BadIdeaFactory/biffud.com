335e1fa7030bff8702d52167f7c3751741db936a
Lou Huang <lou@streetmix.net>
2025-03-22 09:04:36 -0400

    refactor(Projects): port to typescript

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -17,3 +17,6 @@ type PersonNode =
   | Queries.PeopleTplQuery["overlords"]["edges"][0]["node"]
   | Queries.PeopleTplQuery["members"]["edges"][0]["node"]
   | Queries.PeopleTplQuery["accomplices"]["edges"][0]["node"];
+
+type ProjectList = Queries.ProjectsTplQuery["projects"]["edges"];
+type ProjectNode = ProjectList[0]["node"];
